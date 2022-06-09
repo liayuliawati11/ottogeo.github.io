3edbf56222b0f07bc5af89b3874f52c8369e5f87
Lia Yuliawati <liayuliawati@upi.edu>
2022-06-09 12:18:37 +0700

    first commit

---
+++ application/views/layout/viewnav.php
@@ -0,0 +1,23 @@
+<div class="collapse navbar-collapse order-3" id="navbarCollapse">
+        <!-- Left navbar links -->
+
+        <!-- SEARCH FORM -->
+      </div>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+   
+    <!-- /.content-header -->
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
